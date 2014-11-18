e61b7d6da8593063d67caf8a8aac94b86770e83c
Kasper Tvede <kasper@webmasteren.eu>
2014-11-18 14:57:14 +0100

    init commit

---
+++ hhm/hhm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hhm {
+    public class HHM {
+        public int states;
+        public List<int> statesList = new List<int>();
+
+        public int observables = 0;
+        public Dictionary<char, int> obsList = new Dictionary<char, int>();
+
+
+        public double[,] transProbs = new double[0, 0];
+
+        public double[] initProbes = new double[0];
+
+        public double[,] emprobes = new double[0, 0];
+
+        public static HHM readFromFile( string fileName ) {
+            HHM result = new HHM();
+
+            string[] lines = File.ReadAllLines( fileName );
+            int states = int.Parse( lines[1] );
+
+            var stateslist = new List<int>();
+            var split = lines[2].Split( new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries );
+            for (int i = 0; i < states; i++) {
+                stateslist.Add( int.Parse( split[i] ) );
+            }
+
+            var obsCount = int.Parse( lines[4] );
+            var obsDict = new Dictionary<char, int>();
+            split = lines[5].Split( new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries );
+            for (int i = 0; i < obsCount; i++) {
+                obsDict.Add( split[i][0], i );
+            }
+            //init probes
+
+            var initpr = new List<double>();
+            split = lines[7].Split( new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries );
+            for (int i = 0; i < states; i++) {
+                initpr.Add( double.Parse( split[i], CultureInfo.InvariantCulture  ) );
+            }
+
+
+            var transProbes = new double[states, states];
+            for (int i = 0; i < states; i++) {
+                split = lines[9 + i].Split( new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries );
+                int j = 0;
+                foreach (var item in split) {
+                    transProbes[i, j] = double.Parse( item, CultureInfo.InvariantCulture );
+                    j++;
+                }
+            }
+
+            var emProbes = new double[states, obsCount];
+            for (int i = 0; i < states; i++) {
+                split = lines[9 + states + 1 + i].Split( new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries );
+                int j = 0;
+                foreach (var item in split) {
+                    emProbes[i, j] = double.Parse( item, CultureInfo.InvariantCulture );
+                    j++;
+                }
+            }
+
+            result.emprobes = emProbes;
+            result.initProbes = initpr.ToArray();
+            result.states = states;
+            result.statesList = stateslist;
+            result.transProbs = transProbes;
+            result.obsList = obsDict;
+            result.observables = obsCount;
+
+
+            return result;
+        }
+
+    }
+}
---
+++ hhm/viterbi.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hhm {
+    public class viterbi {
+        private String omegaObs;
+        private HHM hm;
+
+        private List<List<double>> calculatedOmega = new List<List<double>>();
+
+        public viterbi( HHM hm ) {
+            this.hm = hm;
+        }
+
+        public void setOmega( String omg ) {
+            omegaObs = omg;
+        }
+
+        public List<List<double>> getOmega() {
+            var result = new List<List<double>>();
+            foreach (var item in omegaObs) {
+                if (result.Count() == 0) {
+                    int upper = hm.emprobes.GetUpperBound( 0 ) + 1;
+                    List<double> temp = new List<double>();
+                    for (int i = 0; i < upper; i++) {
+                        var val = hm.emprobes[i, hm.obsList[item]];
+                        temp.Add( Math.Log( val ) + Math.Log( hm.initProbes[i] ) );
+                    }
+                    result.Add( temp );
+                } else {
+                    //n -1 ..
+                    var last = result.Last();
+
+                    List<double> temp = new List<double>();
+                    for (int i = 0; i < last.Count; i++) {
+                        double maxInner = double.NegativeInfinity;
+                        for (int u = 0; u < last.Count; u++) {
+                            double d = last[u] + Math.Log( hm.transProbs[u, i] );
+                            maxInner = Math.Max( d, maxInner );
+                        }
+                        temp.Add( maxInner + Math.Log( hm.emprobes[i, hm.obsList[item]] ) );
+                    }
+                    result.Add( temp );
+                }
+            }
+            calculatedOmega = result;
+            return result;
+        }
+
+        public string backtrack() {
+
+            var res = backtrack_itt( omegaObs.Length - 1, new List<int>() );
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = res.Count - 1; i >= 0; i--) {
+                string valOfInt = (res[i] + 1).ToString();
+                sb.Append( valOfInt );
+            }
+            return sb.ToString();
+        }
+
+
+        private List<int> backtrack_itt( int start, List<int> z ) {
+            var current = z;
+            for (int i = start; i >= 0; i -= 1) {
+                if (i < 0 && z.Count > 0) {
+                    return current;
+                } else if (current.Count == 0) {
+                    int largestIndex = 0;
+                    double largestVal = double.NegativeInfinity;
+                    for (int j = 0; j < calculatedOmega[i].Count; j++) {
+                        var val = calculatedOmega[i][j];
+                        if (val > largestVal) {
+                            largestVal = val;
+                            largestIndex = j;
+                        }
+                    }
+                    current.Add( largestIndex );
+                } else {
+                    var last = current.Last();
+                    int largestIndex = 0;
+                    double largestSoFar = double.MinValue;
+                    for (int j = 0; j < calculatedOmega[i].Count; j++) {
+                        double val = (calculatedOmega[i][j] + Math.Log( hm.transProbs[j, last] ));
+                        if (val > largestSoFar) {
+                            largestSoFar = val;
+                            largestIndex = j;
+                        }
+                    }
+                    current.Add( largestIndex );
+                }
+            }
+            return z;
+        }
+    }
+}
