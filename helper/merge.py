e65bc7d6c4ee2b21a7a66f15d5e19172d6d93cff
YnkDK <ms@ynk.gl>
2014-11-24 18:24:59 +0100

    Small bug fixes

--- helper/merge.py
+++ helper/merge.py
@@ -3,7 +3,7 @@ from os.path import isfile
 
 try:
 	# Check user input
-	assert len(sys.argv) == 3
+	assert len(sys.argv) in [3,4]
 	assert isfile(sys.argv[1])
 	assert isfile(sys.argv[2])
 except AssertionError:
@@ -14,8 +14,10 @@ with open(sys.argv[1], 'rb') as cFile, open(sys.argv[2], 'rb') as rFile:
 	cAnn = cFile.readlines()
 	rAnn = rFile.readlines()
 	# Annotation came from the same file
-	if cAnn[0] != rAnn[0]:
+	if cAnn[0].strip() != rAnn[0].strip():
 		print 'C-only and R-only file must origin from the same genome'
+		print cAnn[0]
+		print rAnn[0]
 		sys.exit(1)
 	firstLine = cAnn[0]
 	del cAnn[0]
@@ -31,13 +33,19 @@ for i in xrange(len(cAnn)):
 	if len(tmp) == 60:
 		ann.append(tmp + '\r\n')
 		tmp = ''
-		
-	if cAnn[i] == 'C':
+	
+	if cAnn[i] == 'C' and rAnn[i] == 'R':
+		tmp = tmp + 'C' if cAnn[i-1] == 'C' else (tmp + 'R')
+	elif cAnn[i] == 'C':
 		tmp = tmp + 'C'
 	elif rAnn[i] == 'R':
-		result = tmp + 'R'
+		tmp = tmp + 'R'
 	else:
 		tmp = tmp + 'N'
+if tmp != '':
+	ann.append(tmp)
+else:
+	ann[-1] = ann[-1].strip()
 	
-with open('merge.txt', 'wb') as out:
+with open('merge.txt' if len(sys.argv) == 3 else sys.argv[3], 'wb') as out:
 	out.write(''.join(ann))
